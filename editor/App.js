16290de786cdbebc091fd6b955e061a62fad5937
enriquesalas506 <enriquesalas506@gmail.com>
2021-03-05 13:33:24 -0600

    navigation
    navigaiton component

--- editor/App.js
+++ editor/App.js
@@ -16,6 +16,10 @@ import {
   StatusBar,
 } from 'react-native';
 
+import 'react-native-gesture-handler';
+
+import 'react-native-gesture-handler';
+
 import {
   Header,
   LearnMoreLinks,
@@ -24,15 +28,31 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 import MainScreen from './components/MainScreen';
+import NavigationContainer from '@react-navigation/native/src/NavigationContainer';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
+const Stack = createStackNavigator();
+
+function HomeScreen() {
+  return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Home Screen</Text>
+      </View>
+  );
+}
 
 const App: () => React$Node = () => {
   return (
-    <View>
 
-      <MainScreen></MainScreen>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={MainScreen} />
+
+        </Stack.Navigator>
+      </NavigationContainer>
 
 
-    </View>
   );
 };
 
